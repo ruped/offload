4a7e36e121ffc499e519db1723502a728c47e44d
Rupert <rupert@allstreet.org>
2020-04-16 23:13:19 +0100

    Make Pool Fair

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.skipgear/offload "0.1.3"
+(defproject com.skipgear/offload "0.1.4"
   :description "Library to provide alternative ways to share/distribute application workloads."
   :url "https://skipgear.com"
   :license {:name "Eclipse Public License"
